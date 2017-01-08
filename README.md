8a07a9a73331c1189a7b5466ce07d7e383771620
tony2times2times <tony.peraza86@gmail.com>
2017-01-08 17:41:57 -0600

    displaying search results in rows

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ You will need the following:
 
 [] At the top of the page should be a greeting with some direction on how to use the application,
 
-[] Next, should be a inputs where the user can add a new pet with a form,
+[X] Next, should be a inputs where the user can add a new pet with a form,
 
 [] Below all of that should be a list of the entered pets.
 
--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,17 +1,38 @@
 var myApp = angular.module('myApp', []);
 
+
+
 myApp.controller('LaunchpadController', ['$scope', '$http', function($scope, $http) {
     console.log('NG Loaded');
-    //$scope.assignments = [];
 
-    $scope.getData   = function() {
+    $scope.pets = [];
+
+    $scope.view = "small-pic";
+    //toggle info view and picture view
+    $scope.changeClass = function(index) {
+        console.log('Changing pet view for: ' + index );
+        if ($scope.view === "small-pic")
+            $scope.view = "large-pic";
+        else
+            $scope.view = "small-pic";
+    };
+
+    $scope.createIndex = function() {
+        for (var i = 0; i < $scope.pets.length; i++) {
+            $scope.pets[i].index = i;
+        }
+    };
+
+    $scope.getData = function() {
         console.log('getting data from server');
         $http({
             method: 'GET',
             url: '/getPets'
         }).then(function(response) {
             console.log('Response from server: ', response);
-            //$scope.assignments = response.data;
+            $scope.pets = response.data;
+            console.log("there are the pets" + $scope.pets[0].name);
+            $scope.createIndex();
         });
     };
 
@@ -23,6 +44,10 @@ myApp.controller('LaunchpadController', ['$scope', '$http', function($scope, $ht
             age: $scope.age,
             image: $scope.image
         };
+        $scope.name = null;
+        $scope.animal = '';
+        $scope.age = '';
+        $scope.image = '';
         $http({
             method: 'post',
             url: '/postPet',
